34f01084376ba0b29816437fd21515dac6f78faa
Yuheng30 <yzhou.cf@iesesteveterradas.cat>
2023-12-19 16:33:24 +0100

    v.03

--- app/src/main/java/com/example/galeria/MainActivity.java
+++ app/src/main/java/com/example/galeria/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.galeria;
+import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.icu.text.SimpleDateFormat;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
@@ -13,15 +19,31 @@ import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Date;
+
 import android.net.Uri;
+import java.io.FileFilter;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    static Uri photoURI;
+    static String lastPhotoName;
+    static String lastPhotoNameRecord="lastPhoto.txt";
     private ImageView imageView;
     private ActivityResultLauncher<Intent> galleryLauncher;
     private ActivityResultLauncher<Intent> cameraLauncher;
@@ -51,17 +73,14 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onActivityResult(ActivityResult result) {
                         if (result.getResultCode() == RESULT_OK && result.getData() != null) {
-                            handleImageResult(result.getData().getExtras().get("data"));
+                            //handleImageResult(result.getData().getExtras().get("data"));
+                            setPic(photoURI);
                         }
                     }
                 });
 
-        File file = new File(getFilesDir(), "pepe.jpg");
-        System.out.println(file.exists());
-        if (file.exists()){
-            String imagePath = file.getAbsolutePath();
-            imageView.setImageURI(Uri.parse(imagePath));
-        }
+        getLastPhoto();
+
 
         btnOpenGallery.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -85,7 +104,22 @@ public class MainActivity extends AppCompatActivity {
 
     private void takePhoto() {
         Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        cameraLauncher.launch(cameraIntent);
+        File photoFile = null;
+        try {
+            photoFile = createImageFile();
+        } catch (IOException ex) {
+            System.out.println(ex.toString());
+        }
+        // Continue only if the File was successfully created
+        if (photoFile != null) {
+             photoURI = FileProvider.getUriForFile(this,
+                    "com.example.galeria.fileprovider",
+                    photoFile);
+            cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+
+            cameraLauncher.launch(cameraIntent);
+
+        }
     }
 
     private void handleImageResult(Object data) {
@@ -100,12 +134,6 @@ public class MainActivity extends AppCompatActivity {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        } else if (data instanceof Bitmap) {
-            // Cámara
-            Bitmap bitmap = (Bitmap) data;
-            saveImageToInternalStorage(bitmap); // Guardar la imagen en almacenamiento interno
-            imageView.setImageBitmap(bitmap);
-            imageView.setVisibility(View.VISIBLE);
         }
     }
     private void saveImageToInternalStorage(Bitmap bitmap) {
@@ -131,5 +159,45 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+        lastPhotoName=image.getName();
+        return image;
+    }
+
+
+    private void setPic(Uri photoURI) {
+        imageView.setImageURI(photoURI);
+    }
+    private void getLastPhoto() {
+        // Obtener el directorio de imágenes externo
+        File picturesDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+
+        // Obtener la lista de archivos en el directorio de imágenes
+        File[] files = picturesDir.listFiles();
+
+        // Verificar si hay archivos y ordenarlos por fecha de modificación
+        if (files != null && files.length > 0) {
+            Arrays.sort(files, new Comparator<File>() {
+                @Override
+                public int compare(File file1, File file2) {
+                    return Long.compare(file2.lastModified(), file1.lastModified());
+                }
+            });
+
+            Uri uri = Uri.fromFile( files[0]);
+            imageView.setImageURI(uri);
+        } else {
+            System.out.println("mal");
+        }
+    }
 
 }
